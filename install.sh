f295ad200c41be13cc162691f92d25bb04585e0d
Payton Turnage <paytonturnage@gmail.com>
2013-06-24 21:47:45 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+# This will install Charl's dependencies and compile it, assuming you have a
+# compiler. Read before running!
+
+# If you get an issue running it, asking for a shared enet library, remove   
+# that shared enet library file from /usr/local/lib and recompile.
+
+sudo apt-get install libenet-dev libgtk2.0-dev
+wget http://download.dnscrypt.org/libsodium/releases/libsodium-0.4.tar.gz
+tar -xzvf libsodium-0.4.tar.gz
+cd libsodium-0.4
+./configure
+make
+sudo make install
+cd ..
+git clone https://github.com/PaytonTurnage/Charl.git
+cd Charl
+sh unix.sh
+sudo mv client /usr/bin/charl
